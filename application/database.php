6e1e5a04f0ae05a4a688d891b13b53e567673863
five963 <1050103427@qq.com>
2018-06-18 12:02:25 +0800

    ''

--- application/database.php
+++ application/database.php
@@ -13,11 +13,11 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '47.94.21.211',
+    'hostname'        => '47.94.20.211',
     // 数据库名
     'database'        => 'shop',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'wy',
     // 密码
     'password'        => '123456',
     // 端口

b9b70341478b271963922c760137e72ff31a72b6
liqq0313 <liqq0313@163.com>
2018-06-19 10:10:40 +0800

    ''

--- application/index/controller/Category.php
+++ application/index/controller/Category.php
@@ -16,8 +16,10 @@ class Category
 		$data = GoodsCategory::column('category_id,pid,name');
 		$data = Common::getTree($data , 0 , 'category_id');
 		
+		
+		$data = $this->add($data);
+		
 		return json($data);
-		//return json($data);
 		//$data = GoodsCategory::get(6);
 		//var_dump($data->getGoods);
 
@@ -28,7 +30,7 @@ class Category
 	public function add($data){
 		foreach ($data as $key => $value) {
 			if ($value['son']) {
-				$data[$key]['goods'] = $this->goods($value['son'][0]['category_id']);
+				$this->goods($value['son'][0]['category_id']);
 				$this->add($value['son']);
 			}
 		}
@@ -38,6 +40,12 @@ class Category
 
 	public function goods($id){
 		return GoodsCategory::get($id)->getGoods;
+	}
+
+	public function test(){
+		$count = GoodsCategory::get(6)->getGoods;
+		var_dump($count);
+		//var_dump(count($count));
 	}
 	
 }
